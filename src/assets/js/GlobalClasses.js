1cdbb698990183b2baa9caf1562852aa61642149
Griffy <timeo.felix.aux@gmail.Com>
2026-04-08 10:46:56 +0200

    feat: added paralax background component

---
+++ src/assets/js/GlobalClasses.js
@@ -0,0 +1,24 @@
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * @param {nomre 1} a 
+ * @param {nombre 2} b 
+ * @returns la distance en pourcentage entre les deux nombre
+*/
+const distanceToPourcentage = (a, b) => {
+	let result = a - b
+	result = result / b
+	result = result * 100
+	return result
+}
+
+
+export {
+    getRandomInt,
+    distanceToPourcentage,
+};
--- src/components/Background.vue
+++ src/components/Background.vue
@@ -1,7 +1,60 @@
-<template>
+<script setup>
+    import { ref, computed, onMounted } from 'vue';
+    import { getRandomInt, distanceToPourcentage } from '../assets/js/GlobalClasses';
+    import { useMouse, useWindowSize } from '@vueuse/core';
 
-</template>
+    
+    const { x, y, sourceType } = useMouse();
+    const { width, height } = useWindowSize();
+    
 
-<script>
+    const backgroundImages = ref([
+        'lake.gif',
+        'ping_tiled_backgorund.mp4',
+        'unknown_land.gif',
+        'wiggly_tuff_guild_night.gif'
+    ]);
+    
+    const currentBackground = ref('');
+    
+    let paralaxPower = 0.2;
 
+    const imageStyle = computed(() => {
+        const xOffset = distanceToPourcentage(x.value, width.value/2) * paralaxPower * -1;
+        const yOffset = distanceToPourcentage(y.value, height.value/2) * paralaxPower * -1;
+        
+        return {
+            transform: `translate(${xOffset}px, ${yOffset}px)`,
+            width: '120%', 
+            height: '120%',
+            objectFit: 'cover',
+            position: 'absolute',
+            top: '0%',
+            left: '0%',
+            pointerEvents: 'none'
+        };
+    });
+
+    
+    const setRandomBackground = () => {
+        const randomIndex = getRandomInt(0, backgroundImages.value.length - 1);
+        currentBackground.value = backgroundImages.value[randomIndex];
+    };
+
+    
+    onMounted(() => {
+        setRandomBackground();
+    });
 </script>
+
+
+<template>
+    <section class="w-screen h-screen absolute text-white overflow-hidden">
+        <p>{{ x }}</p> <p>{{ y }}</p>
+        <img 
+            :src="`/src/assets/Backgrounds/${currentBackground}`" 
+            :style="imageStyle"
+            alt="Background"
+        >
+    </section>
+</template>
