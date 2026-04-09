c1c8526288918ab07a0ce68c56be617f5f1788b2
Griffy <timeo.felix.aux@gmail.Com>
2026-04-09 11:59:56 +0200

    feat: added button component,
    flags default variable and style is op

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <script setup>
 import Background from './components/Background.vue'
 import debugVar from './components/debugVar.vue'
+import LinkButton from './components/LinkButton.vue'
 
 </script>
 
 <template>
   <debugVar class="absolute" />
-  <section class="overflow-hidden w-screen h-screen absolute top-0 left-0">
-    <Background />
+  <section id="content" class="flex justify-center items-center w-screen h-screen font-[Wondermail]">
+    <LinkButton title="Discord" isActive="true" />
   </section>
+  <div id="bg-wrapper" class="overflow-hidden w-screen h-screen absolute top-0 left-0">
+    <Background />
+  </div>
 </template>
+
+<style>
+@font-face {
+    font-family: "WonderMail";
+    src: url("./assets/fonts/wondermail.ttf") format("truetype");
+    font-weight: normal;
+    font-style: normal;
+    font-display: swap;
+}
+#id {
+  font-family: "WonderMail";
+}
+</style>
---
+++ src/components/LinkButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  title: {type: String, default: "Empty"},
+  isActive: Boolean,
+})
+
+
+</script>
+
+<template>
+    <button v-if="isActive" class="z-1 link-btn responsive-text bg-[#1f1f1f] text-white rounded-2xl">{{ title }}</button>
+</template>
+
+<style>
+.responsive-text{
+    font-size: clamp(1rem, 1.5rem + 0.5vw, 1.7rem);
+    padding: 0.50rem clamp(1.5rem, 3vw, 3rem);
+    width: clamp(300px, 50%, 400px);
+}
+.link-btn {
+    text-shadow: 13px;
+    border: solid #424242 2px;
+    box-shadow: 5px 3px 3px #1f1f1f46;
+
+    transition: 0.2s all ease-out;
+}
+.link-btn:hover{
+    transition: 0.2s all ease-out;
+    transform: scale(1.05);
+    background-color: #424242;
+    border: solid #70a0f8 2px;
+}
+</style>
