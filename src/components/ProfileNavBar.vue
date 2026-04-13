c095d54c349fc261aa74134943666982722efda7
Griffy <timeo.felix.aux@gmail.com>
2026-04-13 20:13:58 +0200

    style: changed top navbar effect

--- src/components/ProfileNavBar.vue
+++ src/components/ProfileNavBar.vue
@@ -1,8 +1,9 @@
 <script setup>
+import { transform } from '@vue/compiler-core';
 import { transition, useScroll } from '@vueuse/core'
 import { computed } from 'vue';
 
-const { x, y, isScrolling, arrivedState } = useScroll(window)
+const { x, y, isScrolling, arrivedState, directions } = useScroll(window)
 
 
 const disapear_onscroll = computed(() => {
@@ -10,15 +11,16 @@ const disapear_onscroll = computed(() => {
 
     return{
         // most animation for now
-        transition: 'all 0.5s ease-in-out',
-        opacity: arrivedState.top ? 1 : 0,
+        transition: 'all 0.2s ease-in-out',
+        transform: arrivedState.top ? 'translate(-50%, 0)' : 'translate(-50%, -150%)',
+
     }
 })
 </script>
 
 <template>
     <div :style="disapear_onscroll" class="fixed-x-center bg-[#424242] border-2 border-[#1f1f1f46] rounded-full w-100 h-15">
-        
+        <p>{{ directions.top }}</p>
     </div>
 </template>
 
