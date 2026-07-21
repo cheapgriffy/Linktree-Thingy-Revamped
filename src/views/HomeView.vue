cf3512c4a51f867275838ddf3b0da90458f2dcc0
Griffy <timeo.felix.aux@gmail.com>
2026-07-21 15:29:04 +0200

    style(homeview) Added placeholder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,13 @@ import ProfilePicture from '../components/ProfilePicture.vue'
 </script>
 
 <template>
-    <div >
-        <ProfilePicture />
+    <div class="flex h-100 w-full">
+        <div class="w-[50%] h-full bg-red-600"></div>
+        <div class="w-[50%] h-full bg-blue-600"></div>
     </div>
+    <footer class="bg-[#2b2223] w-full h-30 rounded-b-4xl">
+        <p>Test content test</p>
+    </footer>
 </template>
 
 
